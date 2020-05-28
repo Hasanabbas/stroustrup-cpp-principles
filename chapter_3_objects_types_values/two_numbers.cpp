3d7ba8088970e50177862b3639bfcd7431ffef16
Hasanabbas Rehemtulla <hasanabbas.rehemtulla@bit-nation.com>
2020-05-28 23:40:44 +0100

    adding exercises 4 and 5

---
+++ chapter_3_objects_types_values/two_numbers.cpp
@@ -0,0 +1,15 @@
+#include "../lib/std_lib_facilities.h"
+
+int main() {
+    cout << "Enter two integers seperated with a space: ";
+    // int val1, val2; // create variables to store user input
+    double val1, val2; // change int to double to see the difference
+    cin >> val1 >> val2; // read in user input and save in variables
+
+    cout << "Smaller number: " << ((val1 < val2) ? val1 : val2) << "\n"
+         << "Larger number: " << ((val1 > val2) ? val1 : val2) << "\n"
+         << "Sum: " << (val1 + val2) << "\n"
+         << "Difference: " << (val1 - val2) << "\n"
+         << "Product: " << (val1 * val2) << "\n"
+         << "Ratio: " << (val1 / val2) << "\n";
+}
