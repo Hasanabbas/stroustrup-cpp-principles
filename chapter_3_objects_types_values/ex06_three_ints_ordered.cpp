726dd67c882c4748174dadf734bc05480d234608
Hasanabbas Rehemtulla <hasanabbas.rehemtulla@bit-nation.com>
2020-05-29 00:00:12 +0100

    adding exercise 6

---
+++ chapter_3_objects_types_values/ex06_three_ints_ordered.cpp
@@ -0,0 +1,18 @@
+#include "../lib/std_lib_facilities.h"
+
+int main() {
+    cout << "Enter three integers seperated by a space: ";
+    int val1, val2, val3; // create variables to store user input
+    cin >> val1 >> val2 >> val3; // read in user input and save in variables
+
+    if (val2 < val1) swap(val2, val1);
+    if (val3 < val2) swap(val3, val2);
+
+    cout << val1 << " " << val2 << " " << val3 << "\n";
+}
+
+void swap(int &x, int &y) {
+    int temp = x;
+    y = x;
+    x = y;
+}
